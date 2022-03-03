7ba7009a9bd067064e12d8bf881396044ff8d238
AllanMazini <aln.mazini@gmail.com>
2022-03-03 15:24:05 -0300

    Finishing the Project

--- main.js
+++ main.js
@@ -1,13 +1,64 @@
-const selector = {
+const selectors = {
     mode: 'section.main .mode',
-    main: 'section.main',
+    section: 'section.main',
     title: 'section.main .title',
     clouds: 'section.main .clouds img'
 }
 
-const mode = document.querySelector(selector.mode);
-const main = document.querySelector(selector.main);
-const title = document.querySelector(selector.title);
-const clouds = document.querySelectorAll(selector.clouds);
+const mode = document.querySelector(selectors.mode);
+const section = document.querySelector(selectors.section);
+const title = document.querySelector(selectors.title);
+const clouds = document.querySelectorAll(selectors.clouds);
 
-console.log(mode, main, title, clouds)
+const MODE_LIGHT = 'light';
+const MODE_DARK = 'dark';
+
+const getCurrentMode = () =>{
+    let currentMode = null;
+    if (section.classList.contains(MODE_LIGHT)) {
+        currentMode = MODE_LIGHT;
+    } else if(section.classList.contains(MODE_DARK)){ 
+        currentMode = MODE_DARK
+    }
+    return currentMode;
+}
+
+const toggleTitle = () =>{
+    const current = getCurrentMode();
+
+    const titleText = {};
+    titleText[MODE_LIGHT] = 'Light';
+    titleText[MODE_DARK] = 'Dark';
+    
+    title.innerText = titleText[current];
+}
+ const toggleMainClass = () =>{
+     const current = getCurrentMode();
+     section.classList.remove(MODE_LIGHT, MODE_DARK);
+     if (current === MODE_LIGHT) {
+        section.classList.add(MODE_DARK)
+     } else  if(current === MODE_DARK){
+         section.classList.add(MODE_LIGHT)
+     }
+ };
+
+ const toggleClouds = () =>{
+    const current = getCurrentMode();
+    const regex = /img\/(dark|light)/;
+    const newSrc = `img/${current}`;
+
+    clouds.forEach((img) => {
+        const imgSrc = img.src.replace(regex, newSrc);
+        img.src = imgSrc;
+    });
+}
+
+    const toggleMode = (e) => {
+        toggleMainClass();
+        toggleTitle();
+        toggleClouds();
+    }
+
+    mode.addEventListener('click', toggleMode);
+    
+
